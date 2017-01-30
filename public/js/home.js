f3a4a976d649da42d055d2949c7e5e2a7465c683
Aviral Bajpai <aviral.bajpai@icloud.com>
2017-01-30 07:39:14 +0530

    assets folder added. orders pages added. login function added to home. orders and products requested on load.

---
+++ public/js/home.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+    //firebase auth handler registration for auth state changed
+    const auth = firebase.auth();
+    auth.onAuthStateChanged(firebaseUser => {
+        console.log("Auth state changed!");
+        console.log(firebaseUser);
+
+        if (!firebaseUser) {
+            window.location = "index.html";
+        }
+    });
+
+    //Fetching Products Data
+    $.ajax({
+        method: "GET",
+        url: "http://alfredproducts.gear.host/api/product"
+    })
+        .done(function (products) {
+            //alert("Data Received: " + JSON.stringify(msg));
+            alert(products.length);
+        });
+
+    //Fetching Orders Data
+    $.ajax({
+        method: "GET",
+        url: "http://alfredorders.gear.host/orders_api.php"
+    })
+        .done(function (orders) {
+            //alert("Data Received: " + JSON.stringify(msg));
+            alert(orders.length);
+        });
+
+});
